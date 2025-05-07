48b7d07abbd0b1b158cdab39ffa64ceb1c9b0051
vschoenknecht <v.schoenknecht@mtg.muenchen.musin.de>
2025-05-07 09:27:51 +0200

    "!"

--- Test.java
+++ Test.java
@@ -1,6 +1,6 @@
 class test{
 public static void main(String args[]){
-    System.out.println("HILFE ICH BIN IN DER KONSOLE GEFANGEN");
+    System.out.println("HILFE ICH BIN IN DER KONSOLE GEFANGEN!");
 }
 
 }
