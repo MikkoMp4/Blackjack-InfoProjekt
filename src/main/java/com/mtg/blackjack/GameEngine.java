c15f6f4b6b88f6cf135873cfb708eddd07ab1c46
Mikko.mp4 <84526459+MikkoMp4@users.noreply.github.com>
2025-05-08 17:04:31 +0200

    2 Basic Java Klasse German Dictonary for spell checking Updaten Maven Dependencies (bibliotheken)

---
+++ src/main/java/com/mtg/blackjack/GameEngine.java
@@ -0,0 +1,7 @@
+package com.mtg.blackjack;
+
+public class GameEngine {
+    public void startConsoleGame() {
+        //hier die Logik für das Konsolen-spiel implementieren
+    }
+}
---
+++ src/main/java/com/mtg/blackjack/Main.java
@@ -0,0 +1,8 @@
+package com.mtg.blackjack;
+
+public class Main {
+    public static void main(String[] args) {
+        GameEngine engine = new GameEngine();
+        engine.startConsoleGame();
+    }
+}
