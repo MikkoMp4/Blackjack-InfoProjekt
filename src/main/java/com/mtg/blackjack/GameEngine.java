b2125c4e62515c3482cddfec3c506db889c00804
Leo B. <161613682+LeoB07@users.noreply.github.com>
2025-05-09 08:37:41 +0200

    Test 6

--- src/main/java/com/mtg/blackjack/GameEngine.java
+++ src/main/java/com/mtg/blackjack/GameEngine.java
@@ -3,5 +3,6 @@ package com.mtg.blackjack;
 public class GameEngine {
     public void startConsoleGame() {
         //hier die Logik für das Konsolen-spiel implementieren
+        System.out.println("gftrmkl");
     }
 }
