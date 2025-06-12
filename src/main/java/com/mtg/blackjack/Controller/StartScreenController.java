208a8903da90e8142b0e8189442d72b819615249
Mikko.mp4 <84526459+MikkoMp4@users.noreply.github.com>
2025-06-12 18:11:48 +0200

    besserer Start screen

--- src/main/java/com/mtg/blackjack/Controller/StartScreenController.java
+++ src/main/java/com/mtg/blackjack/Controller/StartScreenController.java
@@ -1,26 +1,49 @@
 package com.mtg.blackjack.Controller;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
-
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.VBox;
 public class StartScreenController {
 
-    @FXML
-    private Button endlessButton;
+    @FXML private VBox rootVBox;  // das ist unser geankertes VBox
+    @FXML private ImageView logoView;
+    @FXML private HBox buttonBox;
+    @FXML private Button endlessButton;
+    @FXML private Button normalButton;
 
     @FXML
-    private Button normalButton;
+    public void initialize() {
+        // Logo laden
+        logoView.setImage(new Image(getClass().getResourceAsStream("/img/Logo.png")));
 
-    @FXML
-    private void onEndlessClicked(ActionEvent event) {
-        System.out.println("Endlos-Modus gestartet");
-        // TODO: hier den Endlos-Modus starten
+        // Logo auf 50% der Fensterbreite skalieren
+        logoView.fitWidthProperty()
+                .bind(rootVBox.widthProperty().multiply(0.5));
+
+        // Buttons teilen die HBox-Breite gleich auf (abzgl. spacing)
+        endlessButton.prefWidthProperty()
+                .bind(
+                        buttonBox.widthProperty()
+                                .subtract(buttonBox.spacingProperty())
+                                .divide(2)
+                );
+        normalButton.prefWidthProperty()
+                .bind(endlessButton.prefWidthProperty());
     }
 
     @FXML
-    private void onNormalClicked(ActionEvent event) {
+    private void onEndlessClicked() {
+        System.out.println("Endlos-Modus gestartet");
+        // TODO: Endlosmodus starten
+    }
+
+    @FXML
+    private void onNormalClicked() {
         System.out.println("Normaler Modus gestartet");
-        // TODO: hier den Normal-Modus starten
+        // TODO: Normalmodus starten
     }
 }
