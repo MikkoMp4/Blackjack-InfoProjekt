8f6278a59d1c80dd4341845ba6c4a21a9394d6e9
Mikko.mp4 <84526459+MikkoMp4@users.noreply.github.com>
2025-06-12 17:21:41 +0200

    file restructuring + javafx wih scene builder working (blank screen)

--- src/main/java/com/mtg/blackjack/View/HelloFX.java
+++ src/main/java/com/mtg/blackjack/View/HelloFX.java
@@ -5,12 +5,13 @@ import javafx.scene.Scene;
 import javafx.scene.control.Label;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 
 public class HelloFX extends Application {
     @Override
-    public void start(Stage stage) {
-        Label label = new Label("Hallo, JavaFX mit Maven!");
-        StackPane root = new StackPane(label);
+    public void start(Stage stage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/com/mtg/blackjack/View/view.fxml"));
         Scene scene = new Scene(root, 400, 300);
         stage.setScene(scene);
         stage.setTitle("JavaFX Maven App");
